347c7a7563c1a347c4823095b9bbeb3ba4f430e3
Lucas Fernandes Marinho <lucas.fernandes@sga.pucminas.br>
2024-08-21 16:43:13 -0300

    Modificações no Exercicio 1

--- Exercicio1/src/Main/Main.java
+++ Exercicio1/src/Main/Main.java
@@ -7,8 +7,10 @@ public class Main {
 		
 		Scanner scan = new Scanner(System.in);//Cria um novo Scanner
 		
+		System.out.println("Digite um numero:");
 		int n1 = scan.nextInt();//Recebe um numero inteiro e o armazena na variável n1
 		
+		System.out.println("Digite outro numero:");
 		int n2 = scan.nextInt();//Recebe um numero inteiro e o armazena na variável n2
 		
 		int s = n1 + n2;//Realiza a soma e a armazena na variável s
